05f8dafa4feae25875f4eac322a9966de3b112fd
OliviaDemaret <oliviadtqb@gmail.com>
2023-01-18 16:11:21 +0100

    header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
 
-import {createApp} from 'vue';
-import App from './components/App.vue'; //import du composant
-
+import { createApp } from "vue";
+import App from "./components/App.vue"; //import du composant
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 createApp(App) //composant principale vue
-.mount('#app') // monté sur élément DOM id=app
+    .mount("#app"); // monté sur élément DOM id=app
--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,1 +1,70 @@
-<template></template>
+<template>
+    <footer>
+        <div>
+            <img :src="" />
+            <h4>Apocalypse Beauty</h4>
+
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Help & Information</h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Legal</h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Useful links</h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Contact</h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+            <ul id="social media">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <p id="copyright"></p>
+        <div id="payment method">
+            <ul>
+                <li>
+                    <a :href=""><img :src="" /></a>
+                </li>
+                <li>
+                    <a :href=""><img :src="" /></a>
+                </li>
+                <li>
+                    <a :href=""><img :src="" /></a>
+                </li>
+                <li>
+                    <a :href=""><img :src="" /></a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</template>
