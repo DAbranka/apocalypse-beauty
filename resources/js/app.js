def94ebe86ee8a8c8991c059aaebdb8bca1f2d81
Crimsm <c.danniau27@gmail.com>
2023-01-19 15:24:36 +0100

    Test change for sass

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,10 @@ import './bootstrap';
 
 import {createApp} from 'vue';
 import App from './components/App.vue'; //import du composant
+import Header from './components/Header.vue';
+import { create } from 'lodash';
 
 
 createApp(App) //composant principale vue
 .mount('#app') // monté sur élément DOM id=app
+.mount ('#header')
