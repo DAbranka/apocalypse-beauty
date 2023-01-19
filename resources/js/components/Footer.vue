a9f415ddf1d90315a750f835ac6c8b153dc7d6c4
OliviaDemaret <oliviadtqb@gmail.com>
2023-01-19 14:38:49 +0100

    class and id footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,57 +1,57 @@
 <template>
     <footer>
-        <div id="footer">
-        <div class="footer-column">
+        <div id="homepage_footer">
+        <div class="homepage_footer_column">
             <img src="/img/logo/apocalypse-beauty-logo-transparent.png" alt="Apocalypse Beauty Logo">
             <h4>Apocalypse Beauty</h4>
 
-            <ul>
+            <ul class="homepage_footer_ul">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
         </div>
-        <div class="footer-column">
+        <div class="homepage_footer_column">
             <h4>Help & Information</h4>
-            <ul>
+            <ul class="homepage_footer_ul">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
         </div>
-        <div class="footer-column">
+        <div class="homepage_footer_column">
         
             <h4>Legal</h4>
-            <ul>
+            <ul class="homepage_footer_ul">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
         </div>
-        <div class="footer-column">
+        <div class="homepage_footer_column">
             <h4>Useful links</h4>
-            <ul>
+            <ul class="homepage_footer_ul">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
         </div>
-        <div class="footer-column">
+        <div class="homepage_footer_column">
             <h4>Contact</h4>
-            <ul>
+            <ul class="homepage_footer_ul">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
         </div>
-        <div class="footer-column">
-            <ul id="social media">
+        <div class="homepage_footer_column">
+            <ul id="homepage_footer_social media" class="homepage_footer_ul">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
         </div>
-        <p id="copyright"></p>
+        <p id="homepage_copyright"></p>
         <div id="payment method">
             <img
                 src="https://paddleinmastery.com/wp-content/uploads/elementor/thumbs/Payment-Method-logos-p3sjdy67e0tvrapzvo4v20g49x3h6ho46o4ecsisxw.png"
