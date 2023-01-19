4d9b77ec60f03c765f17e08eec591d0f360a8fb5
Zaddes <Blsp.romain@gmail.com>
2023-01-19 14:33:57 +0100

    files - updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
 import './bootstrap';
-
-import {createApp} from 'vue';
-import App from './components/App.vue'; //import du composant
-
-
-createApp(App) //composant principale vue
-.mount('#app') // monté sur élément DOM id=app
