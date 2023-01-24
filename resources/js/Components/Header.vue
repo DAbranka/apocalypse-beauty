40fedfefc5701c7ad77400d2c4a1528731c1a8cf
Crimsm <c.danniau27@gmail.com>
2023-01-24 11:34:49 +0100

    fix footer eand header

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+    
+    <h1>Apocalypse-Beauty</h1>
+
+    <img src="" alt="">
+
+    <ul>
+        <link rel="stylesheet" href="NEWS">
+        <link rel="stylesheet" href="ABOUT">
+        <link rel="stylesheet" href="EXPLORE">
+        <link rel="stylesheet" href="FORUM">
+        <link rel="stylesheet" href="ESHOP">
+    </ul>
+</template>
