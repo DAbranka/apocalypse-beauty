46e056928d40b7f21f03c6d59e4104eaffd21da3
Crimsm <c.danniau27@gmail.com>
2023-01-25 09:56:07 +0100

    fixing

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,26 +1,32 @@
 <script>
-    export default {
-  data() {
-    return {
-      message: 'Hello World!'
-    }
-  }
+export default {
+    data(){
+        return{
+            header_apocalypse:'Apocalypse Beauty',
+            header_news: 'NEWS',
+            
+        }
+    },
+    methods:{
+        header_apocalypseFunction: function () {
+            return this.message;
+        }
+    }   
 }
-
+    
 </script>
-
 <template>
     
-    <h1>Apocalypse-Beauty</h1>
+    <h1 class="">Apocalypse-Beauty</h1>
 
     <img src="" alt="">
 
     <ul>
-        <link rel="stylesheet" href="NEWS">
-        <link rel="stylesheet" href="ABOUT">
-        <link rel="stylesheet" href="EXPLORE">
-        <link rel="stylesheet" href="FORUM">
-        <link rel="stylesheet" href="ESHOP">
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">EXPLORE</a></li>
+        <li><a href="#">FORUM</a></li>
+        <li><a href="#">ESHOP</a></li>
     </ul>
 
     <button><img src="public/img/Icones/user-icones.png" alt=""></button>
