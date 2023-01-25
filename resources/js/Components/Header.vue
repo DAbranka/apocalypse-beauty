60aa580040a5cb3b30fbde2d9e8ce9192354836f
Crimsm <c.danniau27@gmail.com>
2023-01-25 09:20:50 +0100

    trying to fix

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+
+</script>
+
+<template>
+    
+    <h1>Apocalypse-Beauty</h1>
+
+    <img src="" alt="">
+
+    <ul>
+        <link rel="stylesheet" href="NEWS">
+        <link rel="stylesheet" href="ABOUT">
+        <link rel="stylesheet" href="EXPLORE">
+        <link rel="stylesheet" href="FORUM">
+        <link rel="stylesheet" href="ESHOP">
+    </ul>
+
+    <button><img src="public/img/Icones/user-icones.png" alt=""></button>
+
+</template>
