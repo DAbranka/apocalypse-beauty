c378ae36a8dedc504b373128b3d421ead2509115
OliviaDemaret <oliviadtqb@gmail.com>
2023-02-02 11:39:09 +0100

    stream

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -178,7 +178,7 @@ const showingNavigationDropdown = ref(false);
                 <HomepageArticles>
                     <div id="Homepage_hero_wrapper" class=" relative my-20  flex flex-row  text-black justify-center items-center text-center font-aclonica bg-black ">
                             <img id="Homepage_herobanner-bg-image" class=" xs:w-11/12  sm:w-11/12 md:w-11/12 lg:w-7/12 my-25" src="/img/Makeup/illustrations/illustration-makeup-brushes-magical.jpg" >
-                                <div id="homepage-hero-banner-message-box" class=" absolute xs:mt-56 xs:ml-32 sm:mt-72 sm:ml-64 md:mt-80 md:ml-80 lg:mt-80 lg:ml-80 w-60 h-32 text-center flex flex-col justify-center rounded-2xl  bg-pink-600">
+                                <div id="homepage-hero-banner-message-box" class=" absolute xs:mt-28 xs:ml-32 sm:mt-56 sm:ml-64 md:mt-80 md:ml-80 lg:mt-80 lg:ml-80 w-60 h-32 text-center flex flex-col justify-center rounded-2xl  bg-pink-600">
                                     <h3 class="mb-3">Magical moments with makeup</h3>
                                     
                                             <div id="homepage-hero-banner-show-button" class="text-center  text-purple-600 hover:text-pink-600  ">
@@ -196,7 +196,7 @@ const showingNavigationDropdown = ref(false);
 <p class="m-5 text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
         </div>
         <div>
-            <h2 class="text-xl text-bold mb-4 mx-5">Présentation Streameuse</h2>
+            <h2 class="text-xl text-bold mb-4 mx-5">Présentation Marque de cosmétiques</h2>
 <p class="m-5 text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
         </div>
         <div>
