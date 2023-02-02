28a61711ed0fbf9edcb92101f0b30e61d113538c
Crimsm <c.danniau27@gmail.com>
2023-02-02 15:30:19 +0100

    solution error

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import Carousel from '@/Components/Carousel.vue';
-import HomepageArticles from '@/Components/HomepageHero.vue';
+import HomepageHero from '@/Components/HomepageHero.vue';
+import Stream from '@/Components/Stream.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
@@ -12,23 +13,23 @@ import { Head } from '@inertiajs/vue3';
         <template #header>
             <h2 class="font-semibold text-xl text-black leading-tight bg-black">Dashboard</h2>
         </template>
+<HomepageHero>
+    <template #homepageahero>
+
+    </template>
+</HomepageHero>
+
+<Stream>
+    <template #homepagestream>
+
+    </template>
+</Stream>
+
 <Carousel>
 <template #carousel>
 
 </template>
 </Carousel>
-<HomepageHero>
-    <template #homepagearticles>
-
-    </template>
-</HomepageHero>
-
-       <!--  <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
-        </div> -->
+     
     </AuthenticatedLayout>
 </template>
